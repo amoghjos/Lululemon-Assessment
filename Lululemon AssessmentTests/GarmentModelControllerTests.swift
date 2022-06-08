3294eea869afc3c2f1574714589e7435e1a4a166
Amogh Joshi <amogh@craftedthoughts.com>
2022-06-08 13:23:03 -0400

    added testing target and added app icon

---
+++ Lululemon AssessmentTests/GarmentModelControllerTests.swift
@@ -0,0 +1,21 @@
+//
+//  Lululemon_AssessmentTests.swift
+//  Lululemon AssessmentTests
+//
+//  Created by Amogh Joshi on 08/06/22.
+//
+
+import XCTest
+@testable import Lululemon_Assessment
+
+class GarmentModelControllerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func test_add_garment() throws {
+    }
+}
