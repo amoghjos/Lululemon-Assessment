0091299e2b63e9c5c55195131efe09e6986c23c9
Amogh Joshi <amogh@craftedthoughts.com>
2022-06-09 02:21:09 -0400

    initial user interface completed for first screen

--- Lululemon Assessment/Controller/ViewController.swift
+++ Lululemon Assessment/Controller/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
