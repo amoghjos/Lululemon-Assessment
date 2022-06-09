7aabb1395c532273d956cd57cac66679c971a017
Amogh Joshi <amogh@craftedthoughts.com>
2022-06-09 14:09:49 -0400

    added test for no garments for data source

--- Lululemon AssessmentTests/Unit Tests/GarmentViewControllerDataSourceTests.swift
+++ Lululemon AssessmentTests/Unit Tests/GarmentViewControllerDataSourceTests.swift
@@ -11,14 +11,11 @@ import XCTest
 class GarmentViewControllerDataSourceTests: XCTestCase {
 
     var sut: GarmentViewControllerDataSource!
-    var mockedDelegate: GarmentViewControllerDataSourceDelegate!
+    var subDelegate: GarmentViewControllerDataSourceDelegate!
     var tableView: UITableView!
     
     override func setUpWithError() throws {
-        mockedDelegate = GarmentViewControllerDataSourceDelegateStub(K.SampleData.garments)
-        sut = GarmentViewControllerDataSource(delegate: mockedDelegate)
         let vc = try XCTUnwrap(K.storyboard.instantiateViewController(withIdentifier: K.Identifiers.garmentViewController) as? GarmentViewController)
-        
         vc.loadView()
         tableView = vc.tableView
         tableView.dataSource = sut
@@ -27,15 +24,26 @@ class GarmentViewControllerDataSourceTests: XCTestCase {
     override func tearDownWithError() throws {
         sut = nil
         tableView = nil
-        mockedDelegate = nil
+        subDelegate = nil
     }
-   
+    
+    func test_number_of_rows_for_no_garments() throws {
+        subDelegate = GarmentViewControllerDataSourceDelegateStub([])
+        sut = GarmentViewControllerDataSource(delegate: subDelegate)
+        let actualNumberOfRows = sut.tableView(tableView, numberOfRowsInSection: 0)
+        XCTAssertEqual(actualNumberOfRows, 0)
+    }
+    
     func test_number_of_rows_after_adding_few_garments() throws {
+        subDelegate = GarmentViewControllerDataSourceDelegateStub(K.SampleData.garments)
+        sut = GarmentViewControllerDataSource(delegate: subDelegate)
         let actualNumberOfRows = sut.tableView(tableView, numberOfRowsInSection: 0)
         XCTAssertEqual(actualNumberOfRows, K.SampleData.garments.count)
     }
     
     func test_cell_for_rows_after_adding_few_garments() throws {
+        subDelegate = GarmentViewControllerDataSourceDelegateStub(K.SampleData.garments)
+        sut = GarmentViewControllerDataSource(delegate: subDelegate)
         for i in 0..<K.SampleData.garments.count {
             let garment = K.SampleData.garments[i]
             let indexPath = IndexPath(item: i, section: 0)
