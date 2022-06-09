1ed21ad03e9e48e3bd8a321fa26c08ff85d57fa0
Amogh Joshi <amogh@craftedthoughts.com>
2022-06-09 14:21:41 -0400

    Refactored code to remove duplicated code by creating separate functions and variables

--- Lululemon AssessmentTests/Unit Tests/GarmentViewControllerDataSourceTests.swift
+++ Lululemon AssessmentTests/Unit Tests/GarmentViewControllerDataSourceTests.swift
@@ -11,44 +11,52 @@ import XCTest
 class GarmentViewControllerDataSourceTests: XCTestCase {
 
     var sut: GarmentViewControllerDataSource!
-    var subDelegate: GarmentViewControllerDataSourceDelegate!
+    var sutDelegate: GarmentViewControllerDataSourceDelegate!
     var tableView: UITableView!
     
     override func setUpWithError() throws {
-        let vc = try XCTUnwrap(K.storyboard.instantiateViewController(withIdentifier: K.Identifiers.garmentViewController) as? GarmentViewController)
-        vc.loadView()
-        tableView = vc.tableView
+        let vc = K.storyboard.instantiateViewController(withIdentifier: K.Identifiers.garmentViewController)
+        let garmentViewController = try XCTUnwrap(vc as? GarmentViewController)
+        garmentViewController.loadView()
+        tableView = garmentViewController.tableView
         tableView.dataSource = sut
     }
 
     override func tearDownWithError() throws {
         sut = nil
         tableView = nil
-        subDelegate = nil
+        sutDelegate = nil
+    }
+    
+    private let inputGarments = K.SampleData.garments
+    
+    private func setUpDataSource(with garments: [String]) {
+        sutDelegate = GarmentViewControllerDataSourceDelegateStub(garments)
+        sut = GarmentViewControllerDataSource(delegate: sutDelegate)
+    }
+    
+    private func testingNumberOfRows(with garments: [String]) {
+        setUpDataSource(with: garments)
+        let actualNumberOfRows = sut.tableView(tableView, numberOfRowsInSection: 0)
+        XCTAssertEqual(actualNumberOfRows, garments.count)
     }
     
     func test_number_of_rows_for_no_garments() throws {
-        subDelegate = GarmentViewControllerDataSourceDelegateStub([])
-        sut = GarmentViewControllerDataSource(delegate: subDelegate)
-        let actualNumberOfRows = sut.tableView(tableView, numberOfRowsInSection: 0)
-        XCTAssertEqual(actualNumberOfRows, 0)
+        testingNumberOfRows(with: [])
     }
     
-    func test_number_of_rows_after_adding_few_garments() throws {
-        subDelegate = GarmentViewControllerDataSourceDelegateStub(K.SampleData.garments)
-        sut = GarmentViewControllerDataSource(delegate: subDelegate)
-        let actualNumberOfRows = sut.tableView(tableView, numberOfRowsInSection: 0)
-        XCTAssertEqual(actualNumberOfRows, K.SampleData.garments.count)
+    func test_number_of_rows_after_adding_input_garments() throws {
+        testingNumberOfRows(with: inputGarments)
     }
     
-    func test_cell_for_rows_after_adding_few_garments() throws {
-        subDelegate = GarmentViewControllerDataSourceDelegateStub(K.SampleData.garments)
-        sut = GarmentViewControllerDataSource(delegate: subDelegate)
-        for i in 0..<K.SampleData.garments.count {
-            let garment = K.SampleData.garments[i]
+    func test_cell_for_rows_after_adding_input_garments() throws {
+        setUpDataSource(with: inputGarments)
+        for i in 0..<inputGarments.count {
             let indexPath = IndexPath(item: i, section: 0)
             let cell = sut.tableView(tableView, cellForRowAt: indexPath)
             let textLabel = try XCTUnwrap(cell.textLabel)
+            
+            let garment = inputGarments[i]
             XCTAssertEqual(textLabel.text,garment)
         }
     }
