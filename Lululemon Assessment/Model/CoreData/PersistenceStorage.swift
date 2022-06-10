413d06024e5d8488225aeae237d6c46f7e11dc33
Amogh Joshi <amogh@craftedthoughts.com>
2022-06-10 13:14:11 -0400

    added 2 public functions for using Persistent Storage struct

--- Lululemon Assessment/Model/CoreData/PersistenceStorage.swift
+++ Lululemon Assessment/Model/CoreData/PersistenceStorage.swift
@@ -8,6 +8,13 @@
 import Foundation
 import CoreData
 
+//PersistenceStorage is implemented using CoreData and it only exposes 2 APIs to allows other models to interact with it while rest of the implementation details are kept hidden 
 struct PersistenceStorage {
+    static func addGarment(_ name: String){
+        
+    }
     
+    static func getGarments() -> [Garment] {
+        return []
+    }
 }
